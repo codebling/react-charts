eb74eb32787af46db1f17e30c9681729e70b009b
Code Bling <codeblingx@gmail.com>
2021-06-25 03:04:37 -0400

    Convert MyChart to class component

--- examples/custom-styles/src/index.js
+++ examples/custom-styles/src/index.js
@@ -25,7 +25,7 @@ export default class App  extends React.Component {
     } = this.state;
 
     const setState = newState => this.setState(newState);
-    
+
     return (
       <div>
         {JSON.stringify({ activeSeriesIndex, activeDatumIndex }, null, 2)}
@@ -52,31 +52,48 @@ export default class App  extends React.Component {
   }
 }
 
-function MyChart({
-  elementType,
-  activeDatumIndex,
-  activeSeriesIndex,
-  setState
-}) {
+const MyChart = props => {
   useLagRadar();
-
-  const { data, grouping, randomizeData } = useDemoConfig({
+  // const { data, grouping, randomizeData } = useDemoConfig({
+  const demoConfig = useDemoConfig({
     series: 4,
     height: 200,
     grouping: "primary",
     dataType: "ordinal",
     show: ["elementType", "grouping"]
   });
-
-  const series = React.useMemo(
-    () => ({
-      type: elementType
-    }),
-    [elementType]
+  return (
+    <MyChartInner 
+      {...demoConfig}
+      {...props}
+    />
   );
+}
 
-  const axes = React.useMemo(
-    () => [
+class MyChartInner extends React.Component {
+  constructor(props) {
+    super(props);
+  }    
+
+  render() {
+    const {
+      elementType,
+      activeDatumIndex,
+      activeSeriesIndex,
+      setState
+    } = this.props; 
+
+    //useLagRadar();
+
+    
+    const { data, grouping, randomizeData } = this.props;
+  
+
+    const series = {
+      type: elementType
+    };
+
+    const axes = [
       {
         primary: true,
         type: "ordinal",
@@ -87,88 +104,81 @@ function MyChart({
         position: "left",
         stacked: true
       }
-    ],
-    []
-  );
+    ];
 
-  const getSeriesStyle = React.useCallback(
-    series => ({
-      color: `url(#${series.index % 4})`,
-      opacity:
-        activeSeriesIndex > -1
-          ? series.index === activeSeriesIndex
-            ? 1
-            : 0.3
-          : 1
-    }),
-    [activeSeriesIndex]
-  );
+    const getSeriesStyle = 
+      series => ({
+        color: `url(#${series.index % 4})`,
+        opacity:
+          activeSeriesIndex > -1
+            ? series.index === activeSeriesIndex
+              ? 1
+              : 0.3
+            : 1
+      });
 
-  const getDatumStyle = React.useCallback(
-    datum => ({
-      r:
-        activeDatumIndex === datum.index &&
-        activeSeriesIndex === datum.seriesIndex
-          ? 7
-          : activeDatumIndex === datum.index
-          ? 5
-          : datum.series.index === activeSeriesIndex
-          ? 3
-          : datum.otherHovered
-          ? 2
-          : 2
-    }),
-    [activeDatumIndex, activeSeriesIndex]
-  );
+    const getDatumStyle = 
+      datum => ({
+        r:
+          activeDatumIndex === datum.index &&
+          activeSeriesIndex === datum.seriesIndex
+            ? 7
+            : activeDatumIndex === datum.index
+            ? 5
+            : datum.series.index === activeSeriesIndex
+            ? 3
+            : datum.otherHovered
+            ? 2
+            : 2
+      });
 
-  const onFocus = React.useCallback(
-    focused =>
-      setState({
-        activeSeriesIndex: focused ? focused.series.id : -1,
-        activeDatumIndex: focused ? focused.index : -1
-      }),
-    [setState]
-  );
+    const onFocus = 
+      focused =>
+        setState({
+          activeSeriesIndex: focused ? focused.series.id : -1,
+          activeDatumIndex: focused ? focused.index : -1
+        });
 
-  return (
-    <>
-      <button onClick={randomizeData}>Randomize Data</button>
-      <br />
-      <br />
-      <ResizableBox>
-        <Chart
-          data={data}
-          grouping={grouping}
-          series={series}
-          axes={axes}
-          getSeriesStyle={getSeriesStyle}
-          getDatumStyle={getDatumStyle}
-          onFocus={onFocus}
-          tooltip
-          renderSVG={() => (
-            <defs>
-              <linearGradient id="0" x1="0" x2="0" y1="1" y2="0">
-                <stop offset="0%" stopColor="#17EAD9" />
-                <stop offset="100%" stopColor="#6078EA" />
-              </linearGradient>
-              <linearGradient id="1" x1="0" x2="0" y1="1" y2="0">
-                <stop offset="0%" stopColor="#FCE38A" />
-                <stop offset="100%" stopColor="#F38181" />
-              </linearGradient>
-              <linearGradient id="2" x1="0" x2="0" y1="1" y2="0">
-                <stop offset="0%" stopColor="#42E695" />
-                <stop offset="100%" stopColor="#3BB2B8" />
-              </linearGradient>
-              <linearGradient id="3" x1="0" x2="0" y1="1" y2="0">
-                <stop offset="0%" stopColor="#F4Ea0A" />
-                <stop offset="100%" stopColor="#df4081" />
-              </linearGradient>
-            </defs>
-          )}
-        />
-      </ResizableBox>
-    </>
-  );
+    return (
+      <>
+        <button onClick={randomizeData}>Randomize Data</button>
+        <br />
+        <br />
+        <ResizableBox>
+          <Chart
+            data={data}
+            grouping={grouping}
+            series={series}
+            axes={axes}
+            getSeriesStyle={getSeriesStyle}
+            getDatumStyle={getDatumStyle}
+            onFocus={onFocus}
+            tooltip
+            renderSVG={() => (
+              <defs>
+                <linearGradient id="0" x1="0" x2="0" y1="1" y2="0">
+                  <stop offset="0%" stopColor="#17EAD9" />
+                  <stop offset="100%" stopColor="#6078EA" />
+                </linearGradient>
+                <linearGradient id="1" x1="0" x2="0" y1="1" y2="0">
+                  <stop offset="0%" stopColor="#FCE38A" />
+                  <stop offset="100%" stopColor="#F38181" />
+                </linearGradient>
+                <linearGradient id="2" x1="0" x2="0" y1="1" y2="0">
+                  <stop offset="0%" stopColor="#42E695" />
+                  <stop offset="100%" stopColor="#3BB2B8" />
+                </linearGradient>
+                <linearGradient id="3" x1="0" x2="0" y1="1" y2="0">
+                  <stop offset="0%" stopColor="#F4Ea0A" />
+                  <stop offset="100%" stopColor="#df4081" />
+                </linearGradient>
+              </defs>
+            )}
+          />
+        </ResizableBox>
+      </>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
